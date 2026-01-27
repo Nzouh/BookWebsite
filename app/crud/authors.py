d72051f87bea06b9cd36969843e04e9bf3a59e0a
Zouhari <nabil.zouhari@agr.gc.ca>
2026-01-27 15:36:57 -0500

    fixing up the authors create and delete methods

--- app/crud/authors.py
+++ app/crud/authors.py
@@ -1,6 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorDatabase
 from app.db.database import get_db
 from app.model.author import Author
+from bson import ObjectId
 
 
 
@@ -15,5 +16,38 @@ async def create_author(author_data: Author):
 
     result = await collection.insert_one(author)
 
-    return result
+    return  str(result.inserted_id)
+   
     
+async def read_books(_id : str):
+    try:
+        oid = ObjectId(_id)
+    except:
+        raise ValueError("Must be a valid id format")
+    
+    collection = database["authors"]
+
+    print("all books from the author:")
+    author = await collection.find_one({"_id" : oid})
+
+    if author:
+        for book in author.get("book_list", []):
+            print(book)
+    else:
+        print("author not found")
+
+    
+
+async def delete_author(_id : str):
+
+    try:
+        oid = ObjectId(_id)
+    except: 
+        raise ValueError("must be a valid id format")
+    
+    collection = database["authors"]
+    
+    result = await collection.delete_one({"_id": oid})
+
+    return result
+
