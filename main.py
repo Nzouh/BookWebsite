5e52a414389ca7cff68fb9464ad5e1e2f77103af
Nzouh <nzouh055@uottawa.ca>
2026-02-08 22:13:56 -0500

    removing a trailing '@'

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from api.authors import router as author_router
+from api.books import router as book_router
 
 app = FastAPI()
 
 app.include_router(author_router)
+app.include_router(book_router)
 
 @app.get("/")
 def home():
     return {"status" : "Backend is successfully up!"}
-
