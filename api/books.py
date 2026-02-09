aae9eba6d387e0df23196d795bb4e53d97167bec
Nzouh <nzouh055@uottawa.ca>
2026-02-08 22:12:51 -0500

    Completing basic book api implementation

--- api/books.py
+++ api/books.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException, status
-from app.crud.books import create_book, update_book, delete_book, get_book
+from app.crud.books import create_book, update_book, delete_book, get_book, list_books
 from app.model.book import Book
 
 router = APIRouter(prefix="/books", tags=["Books"])
@@ -11,6 +11,23 @@ router = APIRouter(prefix="/books", tags=["Books"])
 async def add_book(book: Book):
     book_id = await create_book(book)
     return {"id": book_id, "status": "created"}
+
+@router.get("/search")
+async def search_books(title: str):
+    # list_books is async, so we must await it
+    book_list = await list_books()
+    searched_title = []
+    
+    for book in book_list:
+        # book is a dictionary, so we access it with ["title"]
+        # using 'in' for partial match, usage of == is also fine but strict
+        if title.lower() in book["title"].lower():
+            searched_title.append(book)
+    
+    if not searched_title:
+        return {"Result": "No books found"}
+        
+    return {"books": searched_title, "result": "found successfully"}
 
 @router.get("/{book_id}")
 async def read_book(book_id: str):
