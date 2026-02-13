24559ef7943889e9ec139a8e1223b9a042ee9248
Nzouh <nzouh055@uottawa.ca>
2026-02-13 02:48:08 -0500

    Removed uused CORS origins

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.authors import router as author_router
 from api.books import router as book_router
 from api.readers import router as reader_router
 from api.auth import router as auth_router
 
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
 app = FastAPI()
-
-origins = [
-    "http://localhost:3000",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
