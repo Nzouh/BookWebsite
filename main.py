50c4ec96870f2d1f243331631559f9ea4ac62414
Nzouh <nzouh055@uottawa.ca>
2026-02-12 23:44:40 -0500

    adding CORS middleware

--- main.py
+++ main.py
@@ -4,7 +4,22 @@ from api.books import router as book_router
 from api.readers import router as reader_router
 from api.auth import router as auth_router
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(author_router)
 app.include_router(book_router)
